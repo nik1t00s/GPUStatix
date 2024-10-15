76ac5b8daa68207284361ad6a6bc3313a7e53bcf
Nikita <nikita.chaika@vk.com>
2024-10-15 15:48:50 +0300

    Add new dependecies and testing OSHI library in Main.java to get hardware data

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,28 @@
 plugins {
     id("java")
     id("application")
+    kotlin("jvm") version "1.8.10"
 }
 
 repositories {
     mavenCentral()
+    maven("https://jcenter.bintray.com/")
+    mavenLocal()
+    maven("https://mvnrepository.com/")
+    maven("https://github.com/")
+    gradlePluginPortal()
 }
 
 dependencies {
     implementation("org.lwjgl:lwjgl:3.3.0")
     implementation("net.java.dev.jna:jna:5.10.0")
+    implementation("org.openjfx:javafx-graphics:20")
+    implementation("org.openjfx:javafx-base:20")
+    implementation("org.openjfx:javafx-controls:17")
+    implementation("org.openjfx:javafx-fxml:17")
+    implementation(kotlin("stdlib"))
+    implementation("net.java.dev.jna:jna:5.10.0")
+    implementation("com.github.oshi:oshi-core:6.4.2")
 }
 
 application {
@@ -21,5 +34,11 @@ tasks.jar {
         attributes(
             "Main-Class" to "com.gpustatix.Main"
         )
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
--- src/main/java/com/gpustatix/Main.java
+++ src/main/java/com/gpustatix/Main.java
@@ -1,7 +1,21 @@
 package com.gpustatix;
 
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.GraphicsCard;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello, GPUStatix!");
+        SystemInfo si = new SystemInfo();
+
+        HardwareAbstractionLayer hal = si.getHardware();
+        OperatingSystem os = si.getOperatingSystem();
+        CentralProcessor cpu = hal.getProcessor();
+
+        System.out.println("Operating System: " + os);
+        System.out.println("CPU: " + cpu);
     }
 }
