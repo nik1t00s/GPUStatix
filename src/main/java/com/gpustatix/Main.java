a0718066fe92fbc08a1f1fcddf8238e4a42cdae5
Nikita <nikita.chaika@vk.com>
2024-10-30 11:28:04 +0300

    Changed IDE to IntelliJ IDEA and new IDEA created new files for Git. None changes in code

--- src/main/java/com/gpustatix/Main.java
+++ src/main/java/com/gpustatix/Main.java
@@ -1,17 +1,10 @@
 package com.gpustatix;
 
 import com.gpustatix.utils.SysInfo;
-import java.util.List;
-import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
-import oshi.hardware.GraphicsCard;
-import oshi.hardware.HardwareAbstractionLayer;
-import oshi.software.os.OperatingSystem;
 
 public class Main {
 
     public static void main(String[] args) {
-        SysInfo sysInfo = new SysInfo();
-        sysInfo.displaySystemInfo();
+        SysInfo.displaySystemInfo();
     }
 }
